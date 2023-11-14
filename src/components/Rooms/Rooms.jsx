ef39d469e7f1fef41c079483b74a5dda2e05cfcf
Ananda Das <65402344+Ananda1405@users.noreply.github.com>
2023-11-14 15:59:44 +0600

    Categories Added

--- src/components/Rooms/Rooms.jsx
+++ src/components/Rooms/Rooms.jsx
@@ -1,20 +1,38 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import Card from "./Card";
+import { useSearchParams } from "react-router-dom";
+import Heading from "../Shared/Heading";
 
 const Rooms = () => {
   const [rooms, setRooms] = useState([]);
 
+  const [params, setParams] = useSearchParams();
+  const category = params.get("category");
+
   useEffect(() => {
     fetch("./rooms.json")
       .then((res) => res.json())
-      .then((data) => setRooms(data));
-  }, []);
+      .then((data) => {
+        if (category) {
+          const filtered = data.filter((room) => room.category === category);
+          setRooms(filtered);
+        } else setRooms(data);
+      });
+  }, [category]);
   return (
-    <div className="grid grid-cols-4 gap-3">
-      {rooms.map((room) => (
-        <Card key={room._id} room={room}></Card>
-      ))}
+    <div>
+      {rooms && rooms.length > 0 ? (
+        <div className="pt-12 grid grid-cols-4 gap-3">
+          {rooms.map((room) => (
+            <Card key={room._id} room={room}></Card>
+          ))}
+        </div>
+      ) : (
+        <div className="flex justify-center items-center min-h-[calc(100vh-300px)]">
+          <Heading center={true} title={"No Rooms Available in this Category."} subtitle={"Please Select Another Category."}></Heading>{" "}
+        </div>
+      )}
     </div>
   );
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
+import Categories from "../../components/Rooms/Category/Categories";
 import Rooms from "../../components/Rooms/Rooms";
 import Container from "../../components/Shared/Container";
 
 const Home = () => {
+
   return (
     <div>
       <Container>
         {/* category  */}
-        
+        <Categories></Categories>
         {/* rooms  */}
         <Rooms></Rooms>
       </Container>
